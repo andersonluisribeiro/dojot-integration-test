e64754d48805108fd2c6b05045e28b5c04883b11
Anderson Luis Ribeiro <anderson_lr@hotmail.com>
2019-02-28 11:16:39 -0300

    checking the same json

--- test/integration/create_template_test.rb
+++ test/integration/create_template_test.rb
@@ -10,11 +10,18 @@ class UserFlowsTest < ActionDispatch::IntegrationTest
           type: "dynamic",
           value_type: "float",
         },
+        {
+          label: "weight",
+          type: "dynamic",
+          value_type: "float",
+        },
       ],
     }
 
-    response = post_data "template", template
+    response = post_data :template, template
     assert response.code, 200
-    assert is_the_same?(template, response.body)
+
+    response_body = parse_json response.body
+    assert same_json?(template, response_body[:template])
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,24 +13,24 @@ class ActiveSupport::TestCase
   # fixtures :all
 
   def post_data(resource, params)
-    return RestClient.post "http://192.168.0.110:8000/#{resource}", params.to_json, {content_type: :json, Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJGVm1OUjQ5djBmVjd4S0huVWRHNlhFZkNRTVFxMGt3VSIsImlhdCI6MTU1MTI2NzM1OCwiZXhwIjoxNTUxMjY3Nzc4LCJwcm9maWxlIjoiYWRtaW4iLCJncm91cHMiOlsxXSwidXNlcmlkIjoxLCJqdGkiOiIyMGM2MTAyNGEzMDAzYWMwZTRmOGQxZTE1M2Q4OWRhNCIsInNlcnZpY2UiOiJhZG1pbiIsInVzZXJuYW1lIjoiYWRtaW4ifQ.ZVXV7LDOzWgwfko7QRNZy4YOikUMFs_hVIkD3_zFKRA"}
+    return RestClient.post "http://192.168.67.117:8000/#{resource.to_s}", params.to_json, {content_type: :json, Authorization: "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJGVm1OUjQ5djBmVjd4S0huVWRHNlhFZkNRTVFxMGt3VSIsImlhdCI6MTU1MTI2NzM1OCwiZXhwIjoxNTUxMjY3Nzc4LCJwcm9maWxlIjoiYWRtaW4iLCJncm91cHMiOlsxXSwidXNlcmlkIjoxLCJqdGkiOiIyMGM2MTAyNGEzMDAzYWMwZTRmOGQxZTE1M2Q4OWRhNCIsInNlcnZpY2UiOiJhZG1pbiIsInVzZXJuYW1lIjoiYWRtaW4ifQ.ZVXV7LDOzWgwfko7QRNZy4YOikUMFs_hVIkD3_zFKRA"}
   end
 
-  def is_the_same?(sent, json_result)
-    result = JSON.parse(json_result)
-    puts "start: #{sent}\n#{json_result}"
+  def same_json?(sent, result, root = nil)
     sent.keys.each do |key|
       if sent[key].is_a?(Array)
         sent[key].each_with_index do |item, index|
-          puts "for: #{item}\n#{result["template"][key.to_s][index]}"
-          is_the_same? item, result["template"][key.to_s][index]
+          same_json? item, result[key][index]
         end
+      else
+        return false unless result[key].eql? sent[key]
       end
-      raise "#{key} is not equal to sent value " unless result["template"][key.to_s].eql? sent[key]
-      return false
     end
     return true
   end
 
-  # Add more helper methods to be used by all tests here...
+  def parse_json(json_result, root = nil)
+    return ActiveSupport::JSON.decode(json_result).deep_symbolize_keys[root] if root
+    return ActiveSupport::JSON.decode(json_result).deep_symbolize_keys
+  end
 end
